4d80c97b14b317ecdbe12295a4c20123f6ae6aa1
Tomas Vondra <tv@fuzzy.cz>
2018-03-06 19:51:08 +0100

    improvements

--- incremental-sort-limit.sh
+++ incremental-sort-limit.sh
@@ -3,162 +3,160 @@
 echo "id scale ngroups work_mem enable_incrementalsort max_workers incremental partial table run duration"
 
 OUT=$1
+SCALE=$2
+
 ID=1
 
-for scale in 100000 1000000 10000000; do
+l=$((SCALE/100))
 
-	l=$((scale/100))
+for ngroups in 10 100 1000 10000; do
 
-	for ngroups in 10 100 1000 10000; do
+	dropdb test
+	createdb test
 
-		dropdb test
-		createdb test
+	psql test -c "CREATE TABLE t (a int, b int, c int, d int, e int)" > /dev/null 2>&1
+	psql test -c "INSERT INTO t SELECT $ngroups*random(), $ngroups*random(), $ngroups*random(), $ngroups*random(), $ngroups*random() FROM generate_series(1,$SCALE)" > /dev/null 2>&1
 
-		psql test -c "CREATE TABLE t (a int, b int, c int, d int, e int)" > /dev/null 2>&1
-		psql test -c "INSERT INTO t SELECT $ngroups*random(), $ngroups*random(), $ngroups*random(), $ngroups*random(), $ngroups*random() FROM generate_series(1,$scale)" > /dev/null 2>&1
+	psql test -c "CREATE TABLE s_1 AS SELECT * FROM t ORDER BY a" > /dev/null 2>&1
+	psql test -c "CREATE TABLE s_2 AS SELECT * FROM t ORDER BY a, b" > /dev/null 2>&1
+	psql test -c "CREATE TABLE s_3 AS SELECT * FROM t ORDER BY a, b, c" > /dev/null 2>&1
+	psql test -c "CREATE TABLE s_4 AS SELECT * FROM t ORDER BY a, b, c, d" > /dev/null 2>&1
 
-		psql test -c "CREATE TABLE s_1 AS SELECT * FROM t ORDER BY a" > /dev/null 2>&1
-		psql test -c "CREATE TABLE s_2 AS SELECT * FROM t ORDER BY a, b" > /dev/null 2>&1
-		psql test -c "CREATE TABLE s_3 AS SELECT * FROM t ORDER BY a, b, c" > /dev/null 2>&1
-		psql test -c "CREATE TABLE s_4 AS SELECT * FROM t ORDER BY a, b, c, d" > /dev/null 2>&1
+	psql test -c "VACUUM FREEZE" > /dev/null 2>&1
+	psql test -c "ANALYZE" > /dev/null 2>&1
 
-		psql test -c "VACUUM FREEZE" > /dev/null 2>&1
-		psql test -c "ANALYZE" > /dev/null 2>&1
+	psql test -c "CHECKPOINT" > /dev/null 2>&1
 
-		psql test -c "CHECKPOINT" > /dev/null 2>&1
+	for mworkers in 0 2; do
 
-		for mworkers in 0 2; do
+		for wm in 4MB 8MB 16MB 32MB 64MB 128MB 256MB; do
 
-			for wm in 4MB 8MB 16MB 32MB 64MB 128MB 256MB; do
+			for incremental in on off; do
 
-				for incremental in on off; do
+				psql test -c "ALTER SYSTEM SET enable_incrementalsort=$incremental" > /dev/null 2>&1
+				psql test -c "ALTER SYSTEM SET work_mem='$wm'" > /dev/null 2>&1
+				psql test -c "ALTER SYSTEM SET max_parallel_workers_per_gather=$mworkers" > /dev/null 2>&1
+				psql test -c "SELECT pg_reload_conf()" > /dev/null 2>&1
 
-					psql test -c "ALTER SYSTEM SET enable_incrementalsort=$incremental" > /dev/null 2>&1
-					psql test -c "ALTER SYSTEM SET work_mem='$wm'" > /dev/null 2>&1
-					psql test -c "ALTER SYSTEM SET max_parallel_workers_per_gather=$mworkers" > /dev/null 2>&1
-					psql test -c "SELECT pg_reload_conf()" > /dev/null 2>&1
+				sql="SELECT * FROM (SELECT * FROM s_1 ORDER BY a) foo ORDER BY a, b LIMIT $l"
 
-					sql="SELECT * FROM (SELECT * FROM s_1 ORDER BY a) foo ORDER BY a, b LIMIT $l"
+				d=`date`
 
-					d=`date`
+				echo "===== $ID [$d] $SCALE $ngroups $wm $incremental $mworkers =====" >> $OUT/explains-limit.log
+				echo "$sql" >> $OUT/explains-limit.log
+				psql test -c "EXPLAIN $sql" >> $OUT/explains-limit.log
 
-					echo "===== $ID [$d] $scale $ngroups $wm $incremental $mworkers =====" >> $OUT/explains-limit.log
-					echo "$sql" >> $OUT/explains-limit.log
-					psql test -c "EXPLAIN $sql" >> $OUT/explains-limit.log
+				incr=`psql test -c "EXPLAIN $sql" | grep 'Incremental Sort' | wc -l`
+				part=`psql test -c "EXPLAIN $sql" | grep 'Partial' | wc -l`
 
-					incr=`psql test -c "EXPLAIN $sql" | grep 'Incremental Sort' | wc -l`
-					part=`psql test -c "EXPLAIN $sql" | grep 'Partial' | wc -l`
+				for r in `seq 1 5`; do
 
-					for r in `seq 1 5`; do
+					if [ -f "stop" ]; then exit; fi
 
-						if [ -f "stop" ]; then exit; fi
+					s=`psql test -t -A -c "select extract(epoch from now())"`
 
-						s=`psql test -t -A -c "select extract(epoch from now())"`
-
-						psql test <<EOF
+					psql test <<EOF
 \o /dev/null
 $sql
 EOF
 
-						d=`psql test -t -A -c "select (1000 * (extract(epoch from now()) - $s))::int"`
+					d=`psql test -t -A -c "select (1000 * (extract(epoch from now()) - $s))::int"`
 
-						echo $ID $scale $ngroups $wm $incremental $mworkers $incr $part s_1 $r $d
-
-					done
-
-					ID=$((ID+1))
-
-					sql="SELECT * FROM (SELECT * FROM s_2 ORDER BY a, b) foo ORDER BY a, b, c LIMIT $l"
-
-					d=`date`
-
-					echo "===== $ID [$d] $scale $ngroups $wm $incremental $mworkers =====" >> $OUT/explains-limit.log
-					echo "$sql" >> $OUT/explains-limit.log
-					psql test -c "EXPLAIN $sql" >> $OUT/explains-limit.log
-
-					incr=`psql test -c "EXPLAIN $sql" | grep 'Incremental Sort' | wc -l`
-					part=`psql test -c "EXPLAIN $sql" | grep 'Partial' | wc -l`
-
-					for r in `seq 1 5`; do
-
-						if [ -f "stop" ]; then exit; fi
-
-						s=`psql test -t -A -c "select extract(epoch from now())"`
-
-						psql test <<EOF
-\o /dev/null
-$sql
-EOF
-
-						d=`psql test -t -A -c "select (1000 * (extract(epoch from now()) - $s))::int"`
-
-						echo $ID $scale $ngroups $wm $incremental $mworkers $incr $part s_2 $r $d
-
-					done
-
-					ID=$((ID+1))
-
-					sql="SELECT * FROM (SELECT * FROM s_3 ORDER BY a, b, c) foo ORDER BY a, b, c, d LIMIT $l"
-
-					d=`date`
-
-					echo "===== $ID [$d] $scale $ngroups $wm $incremental $mworkers =====" >> $OUT/explains-limit.log
-					echo "$sql" >> $OUT/explains-limit.log
-					psql test -c "EXPLAIN $sql" >> $OUT/explains-limit.log
-
-					incr=`psql test -c "EXPLAIN $sql" | grep 'Incremental Sort' | wc -l`
-					part=`psql test -c "EXPLAIN $sql" | grep 'Partial' | wc -l`
-
-					for r in `seq 1 5`; do
-
-						if [ -f "stop" ]; then exit; fi
-
-						s=`psql test -t -A -c "select extract(epoch from now())"`
-
-						psql test <<EOF
-\o /dev/null
-$sql
-EOF
-
-						d=`psql test -t -A -c "select (1000 * (extract(epoch from now()) - $s))::int"`
-
-						echo $scale $ngroups $wm $incremental $mworkers $incr $part s_3 $r $d
-
-					done
-
-					ID=$((ID+1))
-
-					sql="SELECT * FROM (SELECT * FROM s_4 ORDER BY a, b, c, d) foo ORDER BY a, b, c, d, e LIMIT $l"
-
-					d=`date`
-
-					echo "===== $ID [$d] $scale $ngroups $wm $incremental $mworkers =====" >> $OUT/explains-limit.log
-					echo "$sql" >> $OUT/explains-limit.log
-					psql test -c "EXPLAIN $sql" >> $OUT/explains-limit.log
-
-					incr=`psql test -c "EXPLAIN $sql" | grep 'Incremental Sort' | wc -l`
-					part=`psql test -c "EXPLAIN $sql" | grep 'Partial' | wc -l`
-
-					for r in `seq 1 5`; do
-
-						if [ -f "stop" ]; then exit; fi
-
-						s=`psql test -t -A -c "select extract(epoch from now())"`
-
-						psql test <<EOF
-\o /dev/null
-$sql
-EOF
-
-						d=`psql test -t -A -c "select (1000 * (extract(epoch from now()) - $s))::int"`
-
-						echo $scale $ngroups $wm $incremental $mworkers $incr $part s_4 $r $d
-
-					done
-
-					ID=$((ID+1))
+					echo $ID $SCALE $ngroups $wm $incremental $mworkers $incr $part s_1 $r $d
 
 				done
+
+				ID=$((ID+1))
+
+				sql="SELECT * FROM (SELECT * FROM s_2 ORDER BY a, b) foo ORDER BY a, b, c LIMIT $l"
+
+				d=`date`
+
+				echo "===== $ID [$d] $SCALE $ngroups $wm $incremental $mworkers =====" >> $OUT/explains-limit.log
+				echo "$sql" >> $OUT/explains-limit.log
+				psql test -c "EXPLAIN $sql" >> $OUT/explains-limit.log
+
+				incr=`psql test -c "EXPLAIN $sql" | grep 'Incremental Sort' | wc -l`
+				part=`psql test -c "EXPLAIN $sql" | grep 'Partial' | wc -l`
+
+				for r in `seq 1 5`; do
+
+					if [ -f "stop" ]; then exit; fi
+
+					s=`psql test -t -A -c "select extract(epoch from now())"`
+
+					psql test <<EOF
+\o /dev/null
+$sql
+EOF
+
+					d=`psql test -t -A -c "select (1000 * (extract(epoch from now()) - $s))::int"`
+
+					echo $ID $SCALE $ngroups $wm $incremental $mworkers $incr $part s_2 $r $d
+
+				done
+
+				ID=$((ID+1))
+
+				sql="SELECT * FROM (SELECT * FROM s_3 ORDER BY a, b, c) foo ORDER BY a, b, c, d LIMIT $l"
+
+				d=`date`
+
+				echo "===== $ID [$d] $SCALE $ngroups $wm $incremental $mworkers =====" >> $OUT/explains-limit.log
+				echo "$sql" >> $OUT/explains-limit.log
+				psql test -c "EXPLAIN $sql" >> $OUT/explains-limit.log
+
+				incr=`psql test -c "EXPLAIN $sql" | grep 'Incremental Sort' | wc -l`
+				part=`psql test -c "EXPLAIN $sql" | grep 'Partial' | wc -l`
+
+				for r in `seq 1 5`; do
+
+					if [ -f "stop" ]; then exit; fi
+
+					s=`psql test -t -A -c "select extract(epoch from now())"`
+
+					psql test <<EOF
+\o /dev/null
+$sql
+EOF
+
+					d=`psql test -t -A -c "select (1000 * (extract(epoch from now()) - $s))::int"`
+
+					echo $SCALE $ngroups $wm $incremental $mworkers $incr $part s_3 $r $d
+
+				done
+
+				ID=$((ID+1))
+
+				sql="SELECT * FROM (SELECT * FROM s_4 ORDER BY a, b, c, d) foo ORDER BY a, b, c, d, e LIMIT $l"
+
+				d=`date`
+
+				echo "===== $ID [$d] $SCALE $ngroups $wm $incremental $mworkers =====" >> $OUT/explains-limit.log
+				echo "$sql" >> $OUT/explains-limit.log
+				psql test -c "EXPLAIN $sql" >> $OUT/explains-limit.log
+
+				incr=`psql test -c "EXPLAIN $sql" | grep 'Incremental Sort' | wc -l`
+				part=`psql test -c "EXPLAIN $sql" | grep 'Partial' | wc -l`
+
+				for r in `seq 1 5`; do
+
+					if [ -f "stop" ]; then exit; fi
+
+					s=`psql test -t -A -c "select extract(epoch from now())"`
+
+					psql test <<EOF
+\o /dev/null
+$sql
+EOF
+
+					d=`psql test -t -A -c "select (1000 * (extract(epoch from now()) - $s))::int"`
+
+					echo $SCALE $ngroups $wm $incremental $mworkers $incr $part s_4 $r $d
+
+				done
+
+				ID=$((ID+1))
 
 			done
 
--- run.sh
+++ run.sh
@@ -4,11 +4,15 @@ OUT=$1
 
 mkdir $OUT
 
-sh incremental-sort.sh $OUT > $OUT/sort.csv
-sh incremental-sort-limit.sh > $OUT/sort-limit.csv
+for scale in 100000 1000000 10000000; do
 
-sh incremental-sort-indexes.sh > $OUT/sort-indexes.csv
-sh incremental-sort-indexes-limit.sh > $OUT/sort-indexes-limit.csv
+	sh incremental-sort.sh $OUT $scale > $OUT/sort.csv
+	sh incremental-sort-limit.sh $OUT $scale > $OUT/sort-limit.csv
 
-sh incremental-sort-indexes-ios.sh > $OUT/sort-indexes-ios.csv
-sh incremental-sort-indexes-ios-limit.sh > $OUT/sort-indexes-ios-limit.csv
+	sh incremental-sort-indexes.sh $OUT $scale > $OUT/sort-indexes.csv
+	sh incremental-sort-indexes-limit.sh $OUT $scale > $OUT/sort-indexes-limit.csv
+
+	sh incremental-sort-indexes-ios.sh $OUT $scale > $OUT/sort-indexes-ios.csv
+	sh incremental-sort-indexes-ios-limit.sh $OUT $scale > $OUT/sort-indexes-ios-limit.csv
+
+done
