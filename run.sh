f7c30c0a0863152bc7a47e92095b65cab17cdd64
Tomas Vondra <tv@fuzzy.cz>
2018-03-07 23:21:25 +0100

    minor modifications to run.sh

--- run.sh
+++ run.sh
@@ -4,15 +4,18 @@ OUT=$1
 
 mkdir $OUT
 
-for scale in 100000 1000000 10000000; do
 
-	sh incremental-sort.sh $OUT $scale >> $OUT/sort.csv
-	sh incremental-sort-limit.sh $OUT $scale >> $OUT/sort-limit.csv
+for scale in 1000 10000 100000 1000000 10000000; do
 
-	sh incremental-sort-indexes.sh $OUT $scale >> $OUT/sort-indexes.csv
-	sh incremental-sort-indexes-limit.sh $OUT $scale >> $OUT/sort-indexes-limit.csv
+	mkdir $OUT/$scale
 
-	sh incremental-sort-indexes-ios.sh $OUT $scale >> $OUT/sort-indexes-ios.csv
-	sh incremental-sort-indexes-ios-limit.sh $OUT $scale >> $OUT/sort-indexes-ios-limit.csv
+	sh incremental-sort.sh $OUT/$scale $scale > $OUT/$scale/sort.csv
+	sh incremental-sort-limit.sh $OUT/$scale $scale > $OUT/$scale/sort-limit.csv
+
+	sh incremental-sort-indexes.sh $OUT/$scale $scale > $OUT/$scale/sort-indexes.csv
+	sh incremental-sort-indexes-limit.sh $OUT/$scale $scale > $OUT/$scale/sort-indexes-limit.csv
+
+	sh incremental-sort-indexes-ios.sh $OUT/$scale $scale > $OUT/$scale/sort-indexes-ios.csv
+	sh incremental-sort-indexes-ios-limit.sh $OUT/$scale $scale > $OUT/$scale/sort-indexes-ios-limit.csv
 
 done
